771d055f365f6233deffd66028479bd4884380cd
hadidurrani <hadidurrani4@gmail.com>
2023-08-01 23:28:16 +0500

    author udpate and delete before show

--- routes/authors.js
+++ routes/authors.js
@@ -31,13 +31,59 @@ router.post('/', async (req,res)=>{
     })
     try {
         const newAuthor = await author.save()
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect('authors')
+        res.redirect(`authors/${newAuthor.id}`)
     } catch{
         res.render('authors/new' , {
             author: author,
             errorMessage: 'Error Creating Author'
         })
+    }
+})
+
+router.get('/:id', (req,res) =>{
+    res.send('Show Author '+ req.params.id)
+})
+
+router.get('/:id/edit', async(req,res) =>{
+    try{
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit' , {author: author})
+    } catch {
+        res.redirect('/authors')
+    }
+})
+
+router.put('/:id', async (req,res) =>{
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error updating Author'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req,res) =>{
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        await author.deleteOne()
+        res.redirect('/authors')
+    } catch(err) {
+        if(author == null){
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
     }
 })
 
